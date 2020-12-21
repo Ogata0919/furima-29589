a3ba430617fcf99040aa7b9c15cf336eeeae7a40
Ogata0919 <71375611+Ogata0919@users.noreply.github.com>
2020-12-21 17:32:40 +0900

    モデルの単体テストを実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  # before_action :authenticate_user!
+  
 
   private
 
@@ -14,6 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :last_name, :first_name, :last_katakana, :first_katakana, :birthday])
   end
 end
